171fe084994fc082ccd99bd48946e189f091424d
ilias <mail@ilias.xyz>
2019-04-21 23:31:00 +0200

    Tweak build scripts + initial npm distribution config

---
+++ index.js
@@ -0,0 +1,21 @@
+var binwrap = require("binwrap");
+var path = require("path");
+
+var packageInfo = require(path.join(__dirname, "package.json"));
+var version = packageInfo.version;
+var root =
+    "https://github.com/zwilias/elm-json/releases/download/" +
+    version +
+    "/elm-json-" +
+    version;
+
+module.exports = binwrap({
+    dirname: __dirname,
+    binaries: ["myapp-cli"],
+    urls: {
+        "darwin-x64": root + "-x86_64-apple-darwin.tar.gz",
+        "linux-x64": root + "-x86_64-unknown-linux-musl.tar.gz",
+        "win32-x64": root + "-x86_64-pc-windows-msvc.tar.gz",
+        "win32-ia32": root + "-x86_64-pc-windows-msvc.tar.gz"
+    }
+});
