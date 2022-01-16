ec6b836d67f5f7113982a69badbe1d9524b03ca2
ilias <mail@ilias.xyz>
2022-01-16 12:45:46 +0100

    Fix the WD

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ module.exports = binwrap({
     dirname: __dirname,
     binaries: ["elm-json"],
     urls: {
-        "darwin-arm64": root + "-aarch64-apple-darwin.zip",
-        "darwin-x64": root + "-x86_64-apple-darwin.zip",
-        "linux-x64": root + "-x86_64-unknown-linux-musl.zip",
-        "linux-arm": root + "-armv7-unknown-linux-musleabihf.zip",
-        "linux-arm64": root + "-armv7-unknown-linux-musleabihf.zip",
-        "win32-x64": root + "-x86_64-pc-windows-msvc.zip",
-        "win32-ia32": root + "-x86_64-pc-windows-msvc.zip"
+        "darwin-arm64": root + "-aarch64-apple-darwin.tar.gz",
+        "darwin-x64": root + "-x86_64-apple-darwin.tar.gz",
+        "linux-x64": root + "-x86_64-unknown-linux-musl.tar.gz",
+        "linux-arm": root + "-armv7-unknown-linux-musleabihf.tar.gz",
+        "linux-arm64": root + "-armv7-unknown-linux-musleabihf.tar.gz",
+        "win32-x64": root + "-x86_64-pc-windows-msvc.tar.gz",
+        "win32-ia32": root + "-x86_64-pc-windows-msvc.tar.gz"
     }
 });
